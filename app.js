595f900edaf0b934df4f676024b9ccb7d7c2fa67
mageda al-shuriqi <mageda.shuriqi@gmail.com>
2020-12-23 16:10:55 +0200

    new mod

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var countryname = prompt('Tell us, from any country you are?');
 alert('You are welcomed! we sure that '+ countryname + ' is great country')
  var Age = prompt('How old are you?')
  if (Age < 18)
- {alert ('you can have a good information about your school from here')} 
+ {alert ('you are '+ Age +'!'  )} 
  else{ alert('Try to visit us')}
+ var color = prompt('what is your favourite color?')
  confirm ('Dont forget to give us your opinion ^^')
