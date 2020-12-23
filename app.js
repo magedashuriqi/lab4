509e548f714295176743c92de66c21d409e7a115
mageda al-shuriqi <mageda.shuriqi@gmail.com>
2020-12-23 15:34:26 +0200

    new modifing

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('This is my first java script code');
+var countryname = prompt('Tell us, from any country you are?');
+alert('You are welcomed! we sure that '+ countryname + ' is great country')
+ var Age = prompt('How old are you?')
+ if (Age < 18)
+ {alert ('you can have a good information about your school from here')} 
+ else{ alert('Try to visit us')}
+ confirm ('Dont forget to give us your opinion ^^')
