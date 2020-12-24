333068ac3ba28be66a2768aacf21c28c30672e21
mageda al-shuriqi <mageda.shuriqi@gmail.com>
2020-12-24 16:34:25 +0200

    add

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 console.log('This is my first java script code');
 var countryname = prompt('Tell us, from any country you are?');
-alert('You are welcomed! we sure that '+ countryname + ' is great country')
- var Age = prompt('How old are you?')
- if (Age < 18)
- {alert ('you are '+ Age +'!'  )} 
- else{ alert('Try to visit us')}
- var color = prompt('what is your favourite color?')
- confirm ('Dont forget to give us your opinion ^^')
+alert('You are welcomed! we sure that '+ countryname + ' is great country') 
+var Age = prompt('How old are you?')
+ while (Age === '') {Age=prompt('you should tell us about your age!')} 
+ var pic = prompt ('how many picture would you see?')
+ for (i= 1; i <= pic ; i++) { document.write ( '<p>'+ i+'</p>' +'<img width="500",height="1000" src="turkey.jpg">')
+     ;
+     
+ }
+     
+ 
