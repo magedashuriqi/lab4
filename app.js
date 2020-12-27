36809f05769abf98194688ddfb748dec253efb2d
mageda al-shuriqi <mageda.shuriqi@gmail.com>
2020-12-27 16:00:47 +0200

    add

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 console.log('This is my first java script code');
+
 var countryname = prompt('Tell us, from any country you are?');
+
 alert('You are welcomed! we sure that '+ countryname + ' is great country') 
+
 var Age = prompt('How old are you?')
+
  while (Age === '') {Age=prompt('you should tell us about your age!')} 
  var pic = prompt ('how many picture would you see?')
  for (i= 1; i <= pic ; i++) { document.write ( '<p>'+ i+'</p>' +'<img width="500",height="1000" src="turkey.jpg">')
-     ;
-     
+     ;  
  }
      
  
